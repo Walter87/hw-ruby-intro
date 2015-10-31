8db79559812bc6932efce7f125eb36e2f4a27837
walter871 <drunkwolly@gmail.com>
2015-10-31 17:41:06 +0000

    Finish homework after pairprogramming session

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -1,35 +1,61 @@
-# When done, submit this entire file to the autograder.
+# When donesubmit this entire file to the autograder.
 
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  return 0  if arr.empty?
+   arr.inject(:+)
+ 
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  arr=arr.sort.last(2)
+  sum(arr)
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+arr.combination(2).each do |combi|
+  return true if combi.inject(:+) == n
+end
+false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, #{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  return false if s == ''
+  if s.downcase.index(/[qwrtpsdfghjklzxcvbnm]/) == 0
+    true
+  else
+    false
+  end
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+ s.length > 0 && s.match(/^[0|1]*$/) ?  s.to_i(2)%4 ==0 : false
 end
+    
+
 
 # Part 3
 
 class BookInStock
 # YOUR CODE HERE
+  attr_accessor :isbn, :price
+  def initialize(isbn, price)
+    if isbn.eql?("") or price <= 0
+      raise ArgumentError
+    end
+    @isbn =isbn
+    @price = price
+  end
+  
+  def price_as_string
+    "$#{'%.2f' % @price}"
+  end
+      
 end
